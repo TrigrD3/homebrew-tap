873b63d1acd8ead11fc6c72ecdd5a4f8a07d0a92
TrigrD3 <arya66151@gmail.com>
2026-06-21 22:28:50 +0700

    Update terminal-overlay revision hash to 405357b

--- Formula/terminal-overlay.rb
+++ Formula/terminal-overlay.rb
@@ -2,7 +2,7 @@ class TerminalOverlay < Formula
   desc "Zero-dependency floating desktop environment status overlay for Terminal.app"
   homepage "https://github.com/TrigrD3/terminal-overlay"
   url "https://github.com/TrigrD3/terminal-overlay.git",
-      revision: "df8c06fb07d469c3f6183f730cce1f407f952350"
+      revision: "405357b01aded5a31d0306102094f5c43c19981a"
   version "1.0.0"
   license "MIT"
 
